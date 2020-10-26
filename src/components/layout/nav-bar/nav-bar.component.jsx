b1bb5413bf4594e79f63502ed6954219db4f8cf9
Damla Koksal <damlakoksal@outlook.com>
2020-10-26 19:12:36 +0300

    feat: Changed navbar style.

--- src/components/layout/nav-bar/nav-bar.component.jsx
+++ src/components/layout/nav-bar/nav-bar.component.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import './nav-bar.styles.scss';
+import React from "react";
+import { Link } from "react-router-dom";
 
-
+import "./nav-bar.styles.scss";
 
 function NavBar() {
-    
-    return (
-        <div className="navbar-container">
-            
-        </div>
-    )
+  return (
+    <div className="navbar-container">
+      <div className="menu-container align-left">
+        <Link to="/hakkımızda" className="menu-item">
+          HAKKIMIZDA
+        </Link>
+        <Link to="/iletişim" className="menu-item">
+          İLETİŞİM
+        </Link>
+      </div>
+
+      <Link className="logo-container" to="/">
+        <img
+          className="logo"
+          src="/karsal/karsal-logo.png"
+          alt="karsal-logo"
+          width={ 150 }
+          height={ 50 }
+        />
+      </Link>
+
+      <div className="menu-container align-right">
+        <Link to="/fabrika" className="menu-item">
+          FABRİKA
+        </Link>
+        <Link to="/katalog" className="menu-item">
+          KATALOG
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './homepage.styles.scss';
 import NavBar from '../../components/layout/nav-bar/nav-bar.component';
-
+import AnnouncementBar from '../../components/announcement-bar/announcement-bar.component';
 
 export default function HomePage() {
     return (
-        <div className="test">
+        <div className="homepage-container">
+            <AnnouncementBar/>
             <NavBar></NavBar>
         </div>
     )
