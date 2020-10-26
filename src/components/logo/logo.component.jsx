5342bf933ad0983a804a77d961ef7f0534297eab
Damla Koksal <damlakoksal@outlook.com>
2020-10-26 20:11:03 +0300

    docs: components added.

---
+++ src/components/logo/logo.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './logo.styles.scss';
+
+export default function Logo() {
+  return (
+    <Link className="logo-container" to="/">
+      <img
+        className="logo"
+        src="/karsal/karsal-logo.png"
+        alt="karsal-logo"
+        width={150}
+        height={50}
+      />
+    </Link>
+  );
+}
