8fbb416d2d4e2e70c12c6b8a4663058e606bf657
Damla Koksal <damlakoksal@outlook.com>
2020-11-18 20:46:30 +0300

    feat: added conditional styling but it needs optimization

--- src/components/section/section.component.jsx
+++ src/components/section/section.component.jsx
@@ -4,14 +4,15 @@ import "./section.styles.scss";
 
 export default function Section({ BgColor, Background, children }) {
   return (
+    // TOOD: conditional styling will be added
     <section
       className="section-container"
       style={{
-        // backgroundColor: `${BgColor}`,
-        backgroundImage: `url(${Background})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
+        backgroundColor: BgColor && `${BgColor}`,
+        backgroundImage: Background && `url(${Background})`,
+        backgroundPosition: Background && "center",
+        backgroundRepeat: Background && "no-repeat",
+        backgroundSize: Background && "cover",
       }}
     >
       {children}
