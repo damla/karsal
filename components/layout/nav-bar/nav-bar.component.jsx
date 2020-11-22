942232ea8ddd5f1129529ed351ad98854f1b3c7d
Damla Koksal <damlakoksal@outlook.com>
2020-11-22 19:39:10 +0300

    [#8] feat: classNames imported and NavBar style changed

--- components/layout/nav-bar/nav-bar.component.jsx
+++ components/layout/nav-bar/nav-bar.component.jsx
@@ -1,15 +1,29 @@
+import React, { useLayoutEffect } from "react";
+
 import Menu from "../../menu/menu.component";
 import Logo from "../../logo/logo.component";
 import MenuItem from "../../menu-item/menu-item.component";
 
 import "./nav-bar.styles.scss";
 
+import classNames from "classnames";
+
 export default function NavBar() {
+  const [scrolled, setScrolled] = React.useState(false);
+
+  useLayoutEffect(() => {
+    const handleScroll = () =>
+      window.scrollY > 0 ? setScrolled(true) : setScrolled(false);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar-container">
+    <nav className={classNames("navbar-container", { scrolled: scrolled })}>
       <Menu align="left">
         <MenuItem href="/hakkımızda">HAKKIMIZDA</MenuItem>
-
         <MenuItem href="/iletişim">İLETİŞİM</MenuItem>
       </Menu>
       <Logo width="150" height="50" />
