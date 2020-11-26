47bbaf6b36860b461f7da21ca985cb77223c647b
Damla Koksal <damlakoksal@outlook.com>
2020-11-27 00:21:54 +0300

    [#4] feat(content): Added content as parameter

--- components/content/content.component.jsx
+++ components/content/content.component.jsx
@@ -1,31 +1,27 @@
 import "./content.styles.scss";
 
-import BlockQuote from "../block-quote/block-quote.component";
-import CustomButton from "../custom-button/custom-button.component";
-import Logo from "../logo/logo.component";
-
-export default function Content({ children, ...props }) {
+export default function Content({ BgColor, left, title, blockquote, button }) {
     return (
         <div
             className="content-container"
             style={
-                props.BgColor
+                BgColor
                     ? {
-                        backgroundColor: `${props.BgColor}`,
+                        backgroundColor: `${BgColor}`,
                     }
                     : {}
             }
         >
             <div className="content">
-                <div className="content__left">
-                <Logo width={120} height={40} />
-                </div>
+                {left ? (
+                    <div className="content__left">
+                {left}
+            </div>
+                ):null}
                 <div className="content__right">
-                    <h1>Kumaşın Mimarı...</h1>
-                    <BlockQuote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</BlockQuote>
-                    <CustomButton scrollDown>
-                        <div className="button-content"><span>Daha Fazlası</span><span>&#8594;</span></div>
-                    </CustomButton>
+                    <h1>{title}</h1>
+                    {blockquote}
+                    {button}
                 </div>
             </div>
         </div>
