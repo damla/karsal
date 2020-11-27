a7c2173ad6eef3f238ed7d669f59b5f803947df3
Damla Koksal <damlakoksal@outlook.com>
2020-11-27 21:12:44 +0300

    [#4] feat: footer component edited

--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -3,7 +3,11 @@ import "./footer.styles.scss";
 export default function Footer() {
   return (
     <footer className="footer-container">
-      <p>footer</p>
+      <span>Tüm hakları saklıdır 2020 © Karsal Örme A. Ş.</span>
+      <span>
+        <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a> |
+        tel. <a href="tel:+902123456789">+90 (212) 345 67 89</a>
+      </span>
     </footer>
   );
 }
