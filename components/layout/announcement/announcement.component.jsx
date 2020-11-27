7c155fd738411f6ffc18068142cce11d3b713648
Damla Koksal <damlakoksal@outlook.com>
2020-11-27 21:46:15 +0300

    [#4] feat: converted AnnouncementBar component to Bar

--- components/layout/announcement/announcement.component.jsx
+++ components/layout/announcement/announcement.component.jsx
@@ -1,7 +1,11 @@
-import AnnouncementBar from "../../announcement-bar/announcement-bar.component";
-
+import Bar from "../../bar/bar.component";
 import "./announcement.styles.scss";
 
 export default function Announcement() {
-  return <AnnouncementBar />;
+  return (
+    <Bar>
+      <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a>
+      <a href="tel:+902123456789">+90 (212) 345 67 89</a>
+    </Bar>
+  );
 }
--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -1,13 +1,14 @@
-import "./footer.styles.scss";
+import Bar from "../../bar/bar.component";
 
 export default function Footer() {
   return (
-    <footer className="footer-container">
+    <Bar height={"7vh"}>
       <span>Tüm hakları saklıdır 2020 © Karsal Örme A. Ş.</span>
       <span>
-        <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a> |
-        tel. <a href="tel:+902123456789">+90 (212) 345 67 89</a>
+        <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a>
+        &nbsp;|&nbsp;
+        <a href="tel:+902123456789">tel. +90 (212) 345 67 89</a>
       </span>
-    </footer>
+    </Bar>
   );
 }
