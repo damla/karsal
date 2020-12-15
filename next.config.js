d8a3db8bd19db52ca2da8c35bf79509c00beeb51
Damla Köksal <me@damlakoksal.com>
2020-12-16 01:00:54 +0300

    fix: lang fixed

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = withSass({
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["tr-TR", "en-US"],
+    locales: ["tr", "en-US"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en-US",
+    defaultLocale: "tr",
   },
 });
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
           <link rel="stylesheet" href="/assets/fonts/fonts.css" />
         </Head>
