d7566eb30e6e47f92c1c823b81e6e8ec3ea0fcba
Damla Köksal <me@damlakoksal.com>
2020-12-19 22:45:44 +0300

    style(hamburger-button): style edited

---
+++ components/hamburger-button/hamburger-button.component.jsx
@@ -0,0 +1,26 @@
+import "./hamburger-button.style.scss";
+import { useState } from "react";
+import Link from "next/link";
+import classNames from "classnames";
+
+export default function HamburgerButton({ menuActive }) {
+  // const [menuActive, setMenuState] = useState(false);
+
+  // const onClickHandler = () => {
+  //   setMenuState(!menuActive);
+  // };
+
+  return (
+    <li className="hamburger-button__container">
+      <Link href="#">
+        <a
+          className={classNames("menu-item hamburger-button", {
+            "is-active": menuActive,
+          })}
+        >
+          <span className="burger-icon"></span>
+        </a>
+      </Link>
+    </li>
+  );
+}
