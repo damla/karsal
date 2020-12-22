d67458192b89287f4db25d81dfa1959b4e4101fd
Damla Köksal <me@damlakoksal.com>
2020-12-22 20:39:34 +0300

    [#13] refactor(side-bar): sass notation fixed

--- components/layout/side-bar/side-bar.component.jsx
+++ components/layout/side-bar/side-bar.component.jsx
@@ -5,19 +5,26 @@ import "./side-bar.style.scss";
 export default function SideBar({ hamburgerButton, isOpen }) {
   return (
     <div
-      className={classNames("sidebar__container", {
-        "sidebar__container--active": isOpen,
+      className={classNames("sidebar", {
+        "sidebar--active": isOpen,
       })}
     >
-      <div className="sidebar__top">{hamburgerButton}</div>
-      <div className="sidebar__body">
-        <MenuItem href="/hakkımızda">HAKKIMIZDA</MenuItem>
-        <MenuItem href="/iletişim">İLETİŞİM</MenuItem>
-        <MenuItem href="/fabrika">FABRİKA</MenuItem>
-        <MenuItem href="/katalog">KATALOG</MenuItem>
-      </div>
-      <div className="sidebar__footer">
-        <small>Tüm hakları saklıdır. 2020 © Karsal Örme A. Ş.</small>
+      <div className="sidebar__container">
+        <div className="sidebar__top">{hamburgerButton}</div>
+        <div className="sidebar__body">
+          <MenuItem href="/hakkımızda">HAKKIMIZDA</MenuItem>
+          <MenuItem href="/iletişim">İLETİŞİM</MenuItem>
+          <MenuItem href="/fabrika">FABRİKA</MenuItem>
+          <MenuItem href="/katalog">KATALOG</MenuItem>
+        </div>
+        <div className="sidebar__footer">
+          <span>
+            <small>Tüm hakları saklıdır.</small>
+          </span>
+          <span>
+            <small>2020 © Karsal Örme A. Ş.</small>
+          </span>
+        </div>
       </div>
     </div>
   );
