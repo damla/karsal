eb336ee92a2750d49f28043f3d20d7c0d61d09a4
Damla Köksal <me@damlakoksal.com>
2020-12-23 18:28:50 +0300

    [#13] style(sidebar): languages added to menu

--- components/layout/side-bar/side-bar.component.jsx
+++ components/layout/side-bar/side-bar.component.jsx
@@ -12,10 +12,12 @@ export default function SideBar({ hamburgerButton, isOpen }) {
       <div className="sidebar__container">
         <div className="sidebar__top">{hamburgerButton}</div>
         <div className="sidebar__body">
-          <MenuItem href="/hakkımızda">HAKKIMIZDA</MenuItem>
-          <MenuItem href="/iletişim">İLETİŞİM</MenuItem>
+          <MenuItem href="/hakkimizda">HAKKIMIZDA</MenuItem>
+          <MenuItem href="/iletisim">İLETİŞİM</MenuItem>
           <MenuItem href="/fabrika">FABRİKA</MenuItem>
           <MenuItem href="/katalog">KATALOG</MenuItem>
+          <MenuItem href="/">TÜRKÇE</MenuItem>
+          <MenuItem href="/">ENGLISH</MenuItem>
         </div>
         <div className="sidebar__footer">
           <span>
