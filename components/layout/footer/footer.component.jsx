30092c57208aa08dc555a7b55f2f49c2f6f6d3ee
Damla Köksal <me@damlakoksal.com>
2020-12-26 20:53:09 +0300

    #13 fix: reversed the last changes

--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -2,13 +2,15 @@ import Bar from "../../bar/bar.component";
 
 export default function Footer() {
   return (
-    <Bar height={"7vh"}>
-      <span>Tüm hakları saklıdır. 2020 © Karsal Örme A. Ş.</span>
-      <span>
-        <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a>
-        &nbsp;|&nbsp;
-        <a href="tel:+902123456789">+90 (212) 345 67 89</a>
-      </span>
-    </Bar>
+    <div className="footer-container">
+      <Bar height={"7vh"}>
+        <span>Tüm hakları saklıdır. 2020 © Karsal Örme A. Ş.</span>
+        <span>
+          <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a>
+          &nbsp;|&nbsp;
+          <a href="tel:+902123456789">+90 (212) 345 67 89</a>
+        </span>
+      </Bar>
+    </div>
   );
 }
