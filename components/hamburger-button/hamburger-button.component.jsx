f84e9be7dde68c00b8810373f5e682fd7216d081
Damla Köksal <me@damlakoksal.com>
2020-12-28 15:54:18 +0300

    [#32] feat: integrated scss modules to hamburger button component.

--- components/hamburger-button/hamburger-button.component.jsx
+++ components/hamburger-button/hamburger-button.component.jsx
@@ -1,15 +1,15 @@
-import "./hamburger-button.module.scss";
+import styles from  "./hamburger-button.module.scss";
 import Link from "next/link";
 import classNames from "classnames";
 
 export default function HamburgerButton({ menuActive, onClickHandler }) {
   return (
-    <li className="hamburger-button__container" onClick={onClickHandler}>
+    <li className={styles.hamburger-button_container} onClick={onClickHandler}>
       <Link href="#">
         <a
-          className={classNames("menu-item hamburger-button", {
-            "is-active": menuActive,
-          })}
+          className={classNames(styles.menu-item, styles.hamburger-button, 
+            menuActive && styles.hamburger-button__active
+          )}
         >
           <span className="burger-icon"></span>
         </a>
