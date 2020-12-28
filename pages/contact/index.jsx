3d5ee014aafd9b3aa1974646fe6e73ceab2e6194
Damla Köksal <me@damlakoksal.com>
2020-12-28 04:38:52 +0300

    [#31] feat: implemented localization to contact page

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -2,14 +2,31 @@ import Head from "next/head";
 
 import "./contact.styles.scss";
 
-export default function Iletisim() {
+import { connectToDatabase } from "../../util/mongodb";
+
+export async function getStaticProps({ locale }) {
+  const { db } = await connectToDatabase();
+
+  const content = await db.collection("Content").find({}).toArray();
+
+  const t = locale === "tr" ? "İletişim" : "Contact";
+  // console.log(locale);
+
+  return {
+    props: {
+      // content: JSON.stringify(content),
+      content: JSON.stringify(t),
+    },
+  };
+}
+export default function Iletisim({content}) {
   return (
     <>
       <Head>
         <title>İletişim</title>
       </Head>
       <div>
-        <h1>İletişim - TR</h1>
+        <h1>{content}</h1>
       </div>
     </>
   );
