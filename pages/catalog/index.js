78270c191b9ab05f12ba573827f9b5122f7ed5a9
Damla Köksal <me@damlakoksal.com>
2020-12-28 04:35:00 +0300

    [#31] feat: implemented localization to catalog page

--- pages/catalog/index.js
+++ pages/catalog/index.js
@@ -2,14 +2,32 @@ import Head from "next/head";
 
 import "./catalog.styles.scss";
 
-export default function Katalog() {
+import { connectToDatabase } from "../../util/mongodb";
+
+export async function getStaticProps({ locale }) {
+  const { db } = await connectToDatabase();
+
+  const content = await db.collection("Content").find({}).toArray();
+
+  const t = locale === "tr" ? "Katalog" : "Catalog";
+  // console.log(locale);
+
+  return {
+    props: {
+      // content: JSON.stringify(content),
+      content: JSON.stringify(t),
+    },
+  };
+}
+
+export default function Katalog({content}) {
   return (
     <>
       <Head>
         <title>Katalog</title>
       </Head>
       <div>
-        <h1>Katalog - TR</h1>
+        <h1>{content}</h1>
       </div>
     </>
   );
