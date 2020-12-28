b392163d9b1eff9204e086e25657b364350b7e9a
Damla Köksal <me@damlakoksal.com>
2020-12-28 16:37:09 +0300

    [#32] feat: integrated scss modules to menu-item component

--- components/menu-item/menu-item.component.jsx
+++ components/menu-item/menu-item.component.jsx
@@ -7,8 +7,9 @@ export default function MenuItem({ href, children, locale }) {
   return (
     <li>
       <Link href={href} locale={locale}>
-        <a className={"menu-item"}>{children}</a>
+        <a className={styles.menu-item}>{children}</a>
       </Link>
     </li>
   );
 }
+/* TODO: Burayi test et, oncesinde {"classname"} seklinde yazilmis, problem cikarabilirdi */
