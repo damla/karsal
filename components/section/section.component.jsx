a83ac6682ad22f0d182ab8892c6e72f10481aa3f
Damla Köksal <me@damlakoksal.com>
2020-12-28 16:40:06 +0300

    [#32] feat: integrated scss modules to section component

--- components/section/section.component.jsx
+++ components/section/section.component.jsx
@@ -4,9 +4,9 @@ import classNames from "classnames";
 export default function Section({ id, children, banner }) {
   return (
     <section
-      className={classNames("section-container", {
-        "section-container__banner": banner,
-      })}
+      className={classNames(styles.section_container, 
+      banner && styles.section_container__banner
+      )}
       id={id ? id : null}
     >
       {children}
