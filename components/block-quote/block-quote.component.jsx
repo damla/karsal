106360d3742aa4febcdd8897450dcb2126c4fc88
Damla Köksal <me@damlakoksal.com>
2020-12-28 15:42:37 +0300

    [#32] feat: integrated scss modules to content component

--- components/block-quote/block-quote.component.jsx
+++ components/block-quote/block-quote.component.jsx
@@ -2,7 +2,7 @@ import styles from  "./block-quote.module.scss";
 
 export default function BlockQuote({ children }) {
   return (
-    <div className={styles.blockquote-container}>
+    <div className={styles.blockquote_container}>
       <blockquote>{children}</blockquote>
     </div>
   );
