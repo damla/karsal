10661b9c9f4b766f7d004a283d05e102af3d0d12
Damla Köksal <me@damlakoksal.com>
2020-12-28 16:12:21 +0300

    [#32] style: deleted useless className from announcement component

--- components/layout/announcement/announcement.component.jsx
+++ components/layout/announcement/announcement.component.jsx
@@ -1,5 +1,6 @@
 import styles from "./announcement.module.scss";
 
 export default function Announcement({ children }) {
-  return <div className="announcement-container">{children}</div>;
+  return <div>{children}</div>;
 }
+// deleted className="announcement_container"
