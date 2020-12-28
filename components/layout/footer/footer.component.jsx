e732214c812934fd70ed523a21791ea6d93b8942
Damla Köksal <me@damlakoksal.com>
2020-12-28 16:15:28 +0300

    [#32] feat: integrated scss modules to footer component

--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -3,13 +3,13 @@ import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <div className="footer-container">
+    <div className={styles.footer_container}>
       <Bar height={"7vh"}>
-        <div className="footer-container__copyright">
+        <div className={styles.footer_container__copyright}>
           <span>Tüm hakları saklıdır.&nbsp;</span>
           <span>2020 © Karsal Örme A. Ş.</span>
         </div>
-        <span className="footer-container__contact">
+        <span className={styles.footer_container__contact}>
           <a href="mailto:info@karsalorme.com.tr">info@karsalorme.com.tr</a>
           &nbsp;|&nbsp;
           <a href="tel:+902123456789">+90 (212) 345 67 89</a>
