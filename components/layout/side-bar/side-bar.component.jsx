a9dc129f8544396aa3d380f23b96dc38df75ce45
Damla Köksal <me@damlakoksal.com>
2020-12-30 22:56:53 +0300

    [#31] feat: implemented data to sidebar component

--- components/layout/side-bar/side-bar.component.jsx
+++ components/layout/side-bar/side-bar.component.jsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import MenuItem from "../../menu-item/menu-item.component";
 import styles from "./side-bar.module.scss";
 
-export default function SideBar({ hamburgerButton, isOpen }) {
+export default function SideBar({ hamburgerButton, isOpen, sidebar }) {
   return (
     <div
       className={classNames(
@@ -13,31 +13,34 @@ export default function SideBar({ hamburgerButton, isOpen }) {
       <div className={styles.container_content}>
         <div className={styles.container_header}>{hamburgerButton}</div>
         <div className={styles.container_body}>
-          <MenuItem href="/hakkimizda" locale={"tr"}>
-            HAKKIMIZDA
+          <MenuItem
+            href={sidebar.about_us.link}
+            locale={sidebar.about_us.locale}
+          >
+            {sidebar.about_us.text}
           </MenuItem>
-          <MenuItem href="/fabrika" locale={"tr"}>
-            FABRİKA
+          <MenuItem href={sidebar.factory.link} locale={sidebar.factory.locale}>
+            {sidebar.factory.text}
           </MenuItem>
-          <MenuItem href="/katalog" locale={"tr"}>
-            KATALOG
+          <MenuItem href={sidebar.catalog.link} locale={sidebar.catalog.locale}>
+            {sidebar.catalog.text}
           </MenuItem>
-          <MenuItem href="/iletisim" locale={"tr"}>
-            İLETİŞİM
+          <MenuItem href={sidebar.contact.link} locale={sidebar.contact.locale}>
+            {sidebar.contact.text}
           </MenuItem>
-          <MenuItem href="/" locale={"tr"}>
-            TÜRKÇE
+          <MenuItem href={sidebar.turkish.link} locale={sidebar.turkish.locale}>
+            {sidebar.turkish.text}
           </MenuItem>
-          <MenuItem href="/" locale={"tr"}>
-            ENGLISH
+          <MenuItem href={sidebar.english.link} locale={sidebar.english.locale}>
+            {sidebar.english.text}
           </MenuItem>
         </div>
         <div className={styles.container_footer}>
           <span>
-            <small>Tüm hakları saklıdır.&nbsp;</small>
+            <small>{sidebar.footer.text}</small>
           </span>
           <span>
-            <small>2020 © Karsal Örme A. Ş.</small>
+            <small>{sidebar.footer.copyright}</small>
           </span>
         </div>
       </div>
