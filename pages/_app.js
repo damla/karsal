23449b976816961bd5867a41880b135c834bcf43
Damla Köksal <me@damlakoksal.com>
2021-01-01 15:14:08 +0300

    feat: added webVitals to measure performance.

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import "../global/global.scss";
 import smoothscroll from "smoothscroll-polyfill";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function App({ Component, pageProps, router: { locale } }) {
+export function reportWebVitals(metric) {
+  if (metric.label === "custom") {
+    console.log(metric); // The metric object ({ id, name, startTime, value, label }) is logged to the console
+  }
+}
+
+function App({ Component, pageProps, router: { locale } }) {
   if (typeof window !== "undefined") {
     smoothscroll.polyfill();
     localStorage.setItem("lang", locale);
@@ -14,3 +20,4 @@ export default function App({ Component, pageProps, router: { locale } }) {
     </>
   );
 }
+export default App;
