82d5a17c403e65de119a6fe35c8e9a59d1339c63
Damla Köksal <me@damlakoksal.com>
2021-01-01 23:48:34 +0300

    test: test

--- components/menu-item/menu-item.component.jsx
+++ components/menu-item/menu-item.component.jsx
@@ -4,21 +4,21 @@ import styles from "./menu-item.module.scss";
 export default function MenuItem({ href, children, lang, refresh }) {
   // TODO: Ignore warning from here
   return (
-    <>
-      {refresh ? (
-        <li>
-          <a href={href} locale={lang} className={styles.container}>
-            {children}
-          </a>
-        </li>
-      ) : (
-        <li>
-          <Link href={href} locale={lang}>
-            <a className={styles.container}>{children}</a>
-          </Link>
-        </li>
-      )}
-    </>
+    // <>
+    //   {refresh ? (
+    //     <li>
+    //       <a href={href} locale={lang} className={styles.container}>
+    //         {children}
+    //       </a>
+    //     </li>
+    //   ) : (
+    <li>
+      <Link href={href} locale={lang}>
+        <a className={styles.container}>{children}</a>
+      </Link>
+    </li>
+    // )}
+    //   </>
   );
 }
 /* TODO: fix the bug about links */
