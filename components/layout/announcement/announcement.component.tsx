383fecbe770b99f5ba8caed9c207391e669fb16d
Damla Köksal <me@damlakoksal.com>
2021-01-03 01:32:48 +0300

    [#42] feat: typescript added to announcement component

--- components/layout/announcement/announcement.component.tsx
+++ components/layout/announcement/announcement.component.tsx
@@ -1,7 +1,12 @@
+import React, { ReactNode } from "react";
 import styles from "./announcement.module.scss";
 import Bar from "../../bar/bar.component";
 
-export default function Announcement({ children }) {
+interface Props {
+  children: ReactNode;
+}
+
+export default function Announcement({ children }: Props) {
   return (
     <div className={styles.container}>
       <Bar announcement>{children}</Bar>
