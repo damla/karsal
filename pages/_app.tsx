7ef5bf6be5dd209f83a0df04e62deb033fc843f5
Damla Köksal <me@damlakoksal.com>
2021-01-03 22:54:28 +0300

    [#42] feat: typescript added to _app custom file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import "../global/global.scss";
 import smoothscroll from "smoothscroll-polyfill";
+import type { AppProps } from 'next/app';
 
 // This default export is required in a new `pages/_app.js` file.
-export function reportWebVitals(metric) {
+export function reportWebVitals(metric: { label: string; }) {
   if (metric.label === "custom") {
     console.log(metric); // The metric object ({ id, name, startTime, value, label }) is logged to the console
   }
 }
 
-function App({ Component, pageProps, router: { locale } }) {
+function App({ Component, pageProps, router: { locale = "tr" } }: AppProps) {
   if (typeof window !== "undefined") {
     smoothscroll.polyfill();
     localStorage.setItem("lang", locale);
