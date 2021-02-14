3497cc50bd8f59de72c29051d9982de0914aaba9
Damla Köksal <me@damlakoksal.com>
2021-02-14 13:53:49 +0300

    [#70] fix: remove api

--- pages/api/page-images/[pid].ts
+++ pages/api/page-images/[pid].ts
@@ -1,10 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fs from 'fs'
+// import fs from 'fs'
 
+// export default ({ query: { pid } }: NextApiRequest, res: NextApiResponse): void => {
+//   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+//   const val: string = fs.readFileSync(`public/assets/low-quality-images/${pid}`, 'base64')
+
+//   res.status(200).json({ pid: val })
+//   res.end()
+// }
 export default ({ query: { pid } }: NextApiRequest, res: NextApiResponse): void => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  const val: string = fs.readFileSync(`public/assets/low-quality-images/${pid}`, 'base64')
-
-  res.status(200).json({ pid: val })
+  res.status(200).json({ name: ['Hello ', pid] })
   res.end()
 }
